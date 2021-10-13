1827334edb1275828b41294771aa6fac44134fd5
Casper Yonel <yonelcasper@gmail.com>
2021-10-13 09:28:11 -0600

    Challenge completed correctly

---
+++ findTheRange.js
@@ -0,0 +1,9 @@
+// Starting array
+let array = [28, 43, -12, 30, 4, 0, -36]
+
+// Write your solution below:
+let min = Math.min.apply(Math, array);
+    console.log(min);
+
+let max = Math.max.apply(Math, array);
+    console.log(max);
